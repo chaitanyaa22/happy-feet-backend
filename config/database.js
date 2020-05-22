83b7070410916845c465ff7e0b4a102187a3acc6
Chaitanya <ckulkarni120@gmail.com>
2020-05-22 21:37:12 +0530

    fix

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize')
 
 const db = new Sequelize(
-    // 'postgres://xfbzybyt:KLd2zUCIC_S8TZh_0Zq0OwC--6HKT7Oi@rajje.db.elephantsql.com:5432/xfbzybyt', 
-    'happyfeet','postgres','Cck@1832',
+    'postgres://xfbzybyt:KLd2zUCIC_S8TZh_0Zq0OwC--6HKT7Oi@rajje.db.elephantsql.com:5432/xfbzybyt', 
+    // 'happyfeet','postgres','Cck@1832',
     {
     host: 'localhost',
     dialect: 'postgres',
